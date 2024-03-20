5d2a5bc8bb33b3e03bd92c43ffda1f1843498332
Duy Anh NGUYEN <duyanhnguyen08091998@gmail.com>
2024-03-20 12:13:05 +0100

    Changed to function component

--- los-app/src/pages/Game.js
+++ los-app/src/pages/Game.js
@@ -1,119 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import Deck from "../components/Deck";
 
-class Game extends React.Component {
-  constructor(props, token = "") {
-    super(props);
-    this.state = {
-      availableCards: [],
-      deck: [],
-      minimized: false,
-      deckEnd: [],
-      token: token
-    };
-  }
+const Game = ({ token }) => {
+  const [availableCards, setAvailableCards] = useState([]);
+  const [deck, setDeck] = useState([]);
+  const [minimized, setMinimized] = useState(false);
+  const [deckEnd, setDeckEnd] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("http://localhost:3002/cards")
       .then((response) => response.json())
       .then((data) =>
-        this.setState({
-          availableCards: data.map((card, index) => ({
+        setAvailableCards(
+          data.map((card, index) => ({
             ...card,
             originalIndex: index,
-          })),
-        })
+          }))
+        )
       )
       .catch((error) => console.error("Error:", error));
-  }
+  }, []);
 
-  handleCardClick = (card) => {
-    
-      if (this.state.deck.length <= 19){
-        this.setState((prevState) => ({
-          availableCards: prevState.availableCards.filter(
-            (c) => c.id !== card.id
-          ),
-          deck: [...prevState.deck, card],
-          deckEnd : [...prevState.deckEnd, {"key":card.key}]
-          
-        }));
-      }
-    
-    console.log(this.state.token);
+  const handleCardClick = (card) => {
+    if (deck.length <= 19) {
+      setAvailableCards(availableCards.filter((c) => c.id !== card.id));
+      setDeck([...deck, card]);
+      setDeckEnd([...deckEnd, { key: card.key }]);
+    }
+    console.log(token);
   };
 
-  handleRemoveCard = (card) => {
-    this.setState((prevState) => {
-      const newAvailableCards = [...prevState.availableCards, card].sort(
-        (a, b) => a.originalIndex - b.originalIndex
-      );
-      console.log(this.state.deck);
-
-      return {
-        deck: prevState.deck.filter((c) => c.id !== card.id),
-        availableCards: newAvailableCards,
-      };
-    });
-  };
-  handleMinimizeClick = () => {
-    this.setState((prevState) => ({
-      minimized: !prevState.minimized,
-    }));
-  };
-
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row ">
-          {this.state.minimized && (
-            <button
-              type="button"
-              className="btn btn-primary m-3 fs-2"
-              style={{
-                position: "fixed",
-                width: "100px",
-                height: "100px",
-                right: "0",
-                bottom: "0",
-                zIndex: "1000",
-                borderRadius: "50%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              {this.state.deck.length}
-            </button>
-          )}
-
-          <button
-            onClick={this.handleMinimizeClick}
-            className="fixed-bottom btn btn-danger m-3"
-            style={{ width: "50px", height: "50px" }}
-          >
-            {this.state.minimized ? "+" : "-"}
-          </button>
-          <div className="col-md-12">
-            <div className="row no-gutters justify-content-center">
-              {this.state.availableCards.map((card) => (
-                <Card
-                  key={card.id}
-                  card={card}
-                  onClick={() => this.handleCardClick(card)}
-                />
-              ))}
-            </div>
-          </div>
-
-          {!this.state.minimized && (
-            <Deck cards={this.state.deck} deck={this.state.deckEnd} onCardClick={this.handleRemoveCard} token={this.state.token} />
-          )}
-        </div>
-      </div>
+  const handleRemoveCard = (card) => {
+    const newAvailableCards = [...availableCards, card].sort(
+      (a, b) => a.originalIndex - b.originalIndex
     );
-  }
-}
+    console.log(deck);
+
+    setDeck(deck.filter((c) => c.id !== card.id));
+    setAvailableCards(newAvailableCards);
+  };
+
+  const handleMinimizeClick = () => {
+    setMinimized(!minimized);
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row ">
+        {minimized && (
+          <button
+            type="button"
+            className="btn btn-primary m-3 fs-2"
+            style={{
+              position: "fixed",
+              width: "100px",
+              height: "100px",
+              right: "0",
+              bottom: "0",
+              zIndex: "1000",
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {deck.length}
+          </button>
+        )}
+
+        <button
+          onClick={handleMinimizeClick}
+          className="fixed-bottom btn btn-danger m-3"
+          style={{ width: "50px", height: "50px" }}
+        >
+          {minimized ? "+" : "-"}
+        </button>
+        <div className="col-md-12">
+          <div className="row no-gutters justify-content-center">
+            {availableCards.map((card) => (
+              <Card
+                key={card.id}
+                card={card}
+                onClick={() => handleCardClick(card)}
+              />
+            ))}
+          </div>
+        </div>
+
+        {!minimized && (
+          <Deck
+            cards={deck}
+            deck={deckEnd}
+            onCardClick={handleRemoveCard}
+            token={token}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Game;
