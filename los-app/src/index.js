1f5b27c356da9f591bed50ab796cac3c6b3b0c4a
Duy Anh NGUYEN <duyanhnguyen08091998@gmail.com>
2024-03-29 10:07:36 +0100

    Remove unnecessary props in Match component

--- los-app/src/index.js
+++ los-app/src/index.js
@@ -58,7 +58,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/match",
-        element: <Match username={username} token={token}/>,
+        element: <Match/>,
       },
     ],
   },
